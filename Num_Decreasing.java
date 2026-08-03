7466a578cb1d80f6df42b66782e313de0b21a688
MANOJ M <manojnr38@gmail.com>
2026-08-03 10:16:06 +0530

    Add files via upload

---
+++ Num_Decreasing.java
@@ -0,0 +1,10 @@
+public class Num_Decreasing {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        int num = 10;
+        while (num > 0) {
+            System.out.println(num);
+            num--;
+        }
+    }
+}
---
+++ Num_Increasing.java
@@ -0,0 +1,10 @@
+public class Num_Increasing {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        int num = 0;
+        while (num < 10) {
+            System.out.println(num);
+            num++;
+        }
+    }
+}

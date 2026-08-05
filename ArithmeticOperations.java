20c462bacab910e0cb98880890ce0a10d52fc671
MANOJ M <manojnr38@gmail.com>
2026-08-05 18:43:05 +0530

    Add files via upload

---
+++ ArithmeticOperations.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class ArithmeticOperations {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Input two numbers
+        System.out.print("Enter first number: ");
+        double num1 = sc.nextDouble();
+
+        System.out.print("Enter second number: ");
+        double num2 = sc.nextDouble();
+
+        // Arithmetic operations
+        System.out.println("Addition = " + (num1 + num2));
+        System.out.println("Subtraction = " + (num1 - num2));
+        System.out.println("Multiplication = " + (num1 * num2));
+
+        if (num2 != 0) {
+            System.out.println("Division = " + (num1 / num2));
+        } else {
+            System.out.println("Division is not possible (cannot divide by zero).");
+        }
+
+        sc.close();
+    }
+}

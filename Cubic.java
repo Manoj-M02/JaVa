e6b4c605088f7aff3f3b73d2b477ea3c88812844
MANOJ M <manojnr38@gmail.com>
2026-08-13 23:15:22 +0530

    Add files via upload

---
+++ Cubic.java
@@ -0,0 +1,12 @@
+ public class Cubic {
+    public static void main(String[] args) {
+        int n=5,m=4,o=3;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                for(int k=1;k<=o;k++){
+                    System.out.println(  i + " " + j + " " + k);
+                }
+            }
+        }
+    }    
+}

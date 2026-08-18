7ef4b31d16bfa5fc8d1699bc6c0169187bb0ff28
MANOJ M <manojnr38@gmail.com>
2026-08-18 12:32:22 +0530

    Add files via upload

---
+++ Factorial_Time.java
@@ -0,0 +1,14 @@
+public class Factorial_Time {
+    public static void main(String[] args) {
+        permute("ABC", "");
+    }
+    static void permute(String str, String result) {
+        if (str.isEmpty()) {
+            System.out.println(result);
+            return;
+        }
+        for (int i = 0; i < str.length(); i++) {
+            permute(str.substring(0, i) + str.substring(i + 1), result + str.charAt(i));
+        }
+    }
+}

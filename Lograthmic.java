813b5a6f6f0f8cc2e5f56fbfc4619631d2131883
MANOJ M <manojnr38@gmail.com>
2026-08-18 12:33:26 +0530

    Add files via upload

---
+++ Lograthmic.java
@@ -0,0 +1,9 @@
+//O(logn) Lograthmic Time Complexity
+public class Lograthmic {
+    public static void main(String[] args) {
+        int n = 100;
+        for (int i = 1; i <= n; i=i*3) {
+            System.out.println(i+"");
+        }
+    }
+}
